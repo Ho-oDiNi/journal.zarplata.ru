bf3a4cc2e056a2bb1ea21beb56dc54d7d42e70d0
Огнев Юрий Юрьевич <yu.ognev@s7.ru>
2023-11-26 19:33:30 +0700

    add dark theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,7 +159,7 @@ var callback = function () {
     const urlToShare = window.location.href;
     const baseURL = link;
     const params = `${encodeURIComponent(urlToShare)}`;
-    ym(77659420,'reachGoal','sharePost');
+    ym(77659420, "reachGoal", "sharePost");
 
     const shareWindow = window.open(`${baseURL}${params}`);
     shareWindow.focus();
